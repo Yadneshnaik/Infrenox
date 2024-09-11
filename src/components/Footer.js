f1e4e066324de981c100a921bc1ede8b582e4117
Yadneshnaik <naikyadnesh9@gmail.com>
2024-09-11 17:06:05 +0530

    init

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 IT Consultancy. All rights reserved.</p>
+      <p>&copy; 2024 Infrenox Pvt Ltd. All rights reserved.</p>
     </footer>
   );
 }
