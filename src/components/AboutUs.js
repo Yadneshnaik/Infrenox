e725d9e6c415fa4e8ba4b8694eebb2f127595057
Yadneshnaik <naikyadnesh9@gmail.com>
2024-09-11 17:04:02 +0530

    First Commit

---
+++ src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './AboutUs.css';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <h2>About Us</h2>
+      <p>We are a leading IT consultancy firm with years of experience in delivering quality solutions to businesses.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
